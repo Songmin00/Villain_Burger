a6d92de65c63de47a426b201a1e87b46d064c6a1
Songmin00 <dn7272@gmail.com>
2026-06-14 03:07:53 +0900

    feat : CameraController, InputHandler
    버튼에 따른 카메라 이동 기능 구현

---
+++ Assets/Scripts/Manager/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _cameraSpeed;
+    [SerializeField] private Vector3[] _targetPos;
+    public int CurrentFocus {  get; private set; }
+    public int IndexLength { get; private set; }
+    
+    private Camera _camera;
+    private Vector3 _currentTargetPos;
+
+    private void Awake()
+    {
+        CurrentFocus = 0;
+        IndexLength = _targetPos.Length;
+    }
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 currentPos = _camera.transform.position;
+        
+        if (Vector3.Distance(currentPos, _currentTargetPos) < 0.01f)
+        {            
+            _camera.transform.position = _currentTargetPos;
+            
+            this.enabled = false;
+            return;
+        }        
+        _camera.transform.position = Vector3.Lerp(currentPos, _currentTargetPos, _cameraSpeed * Time.deltaTime);
+    }    
+
+    public void SetTargetPos(int index)
+    {
+        _currentTargetPos = _targetPos[index];
+        CurrentFocus = index;
+    }
+}
---
+++ Assets/Scripts/Manager/InputHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] CameraController _cameraController;
+
+    public void OnDownButtonClick()
+    {
+        if (_cameraController == null)
+        {
+            Debug.LogError("CameraController가 할당되지 않았습니다.");
+            return;
+        }
+        if (_cameraController.CurrentFocus < _cameraController.IndexLength -1)
+        {
+            SetCameraFocus(_cameraController.CurrentFocus + 1);
+        }
+        else
+        {
+            return;
+        }        
+    }
+
+    public void OnUpButtonClick()
+    {
+        if (_cameraController == null)
+        {
+            Debug.LogError("CameraController가 할당되지 않았습니다.");
+            return;
+        }
+        if (_cameraController.CurrentFocus > 0)
+        {
+            SetCameraFocus(_cameraController.CurrentFocus - 1);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void SetCameraFocus(int index)
+    {
+        _cameraController.enabled = true;
+        _cameraController.SetTargetPos(index);
+    }
+
+    
+}
