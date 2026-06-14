93f05e0a138ebaf22376286e27d92fad754b1397
Songmin00 <dn7272@gmail.com>
2026-06-14 17:54:17 +0900

    feat : UpDownButtonConnecter
    버튼 온클릭 이벤트와 카메라 이동 로직 간 연결을 담당할 커넥터 클래스 구현.

---
+++ Assets/Scripts/UI/UpDownButtonConnecter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 실제 버튼과 InputHandler를 연결하는 커넥터.
+/// 이벤트 연결 및 버튼 활성/비활성화 담당
+/// </summary>
+
+public class UpDownButtonConnecter : MonoBehaviour
+{
+    [SerializeField] InputHandler _handler;
+
+    [SerializeField] Button _upButton;
+    [SerializeField] Button _downButton;    
+
+    private void Start()
+    {
+        _upButton.onClick.RemoveAllListeners();
+        _downButton.onClick.RemoveAllListeners();
+
+        _upButton.onClick.AddListener(OnUpButtonClick);
+        _downButton.onClick.AddListener(OnDownButtonClick);
+
+        OnUpButtonClick();
+    }
+
+    private void OnDestroy()
+    {
+        _upButton.onClick.RemoveAllListeners();
+        _downButton.onClick.RemoveAllListeners();
+    }
+
+    private void OnUpButtonClick()
+    {
+        _handler.OnUpButtonClick();
+        _upButton.interactable = false;
+        _downButton.interactable = true;
+    }
+
+    private void OnDownButtonClick()
+    {
+        _handler.OnDownButtonClick();
+        _downButton.interactable = false;
+        _upButton.interactable = true;
+    }
+}
